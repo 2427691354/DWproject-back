feb8b4b478856c80916062f9f4a035ff6c027ac7
cc <2427691354@qq.com>
2020-01-06 12:25:04 +0800

    初始

---
+++ src/main/java/com/example/gdjtback/GdjtBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.gdjtback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdjtBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdjtBackApplication.class, args);
+    }
+
+}
