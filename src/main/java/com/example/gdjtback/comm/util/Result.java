018b8ebcd209d7abf618572ca40235fc76a66dcb
cc <2427691354@qq.com>
2020-01-07 17:52:56 +0800

    统一返回结果

---
+++ src/main/java/com/example/gdjtback/comm/util/Result.java
@@ -0,0 +1,19 @@
+package com.example.gdjtback.comm.util;
+
+import lombok.Data;
+
+/**
+ * http请求返回的最外层对象
+ * Created by cc on 2020/1/7.
+ */
+@Data
+public class Result<T> {
+    /** 错误码. */
+    private Integer code;
+
+    /** 提示信息. */
+    private String msg;
+
+    /** 具体的内容. */
+    private T data;
+}
