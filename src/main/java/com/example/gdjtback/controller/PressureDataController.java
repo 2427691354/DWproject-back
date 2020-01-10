30c51d3dbf1ff8c91a8549455ad4aa1d378adb93
jichenzheng <1419275033@qq.com>
2020-01-10 12:40:31 +0800

    测试

--- src/main/java/com/example/gdjtback/controller/PressureDataController.java
+++ src/main/java/com/example/gdjtback/controller/PressureDataController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Api(value = "PressureDataController",tags = {"传感器气瓶气压数据"})
-@RequestMapping("/PressureData")
+
 public class PressureDataController {
 
     @Autowired
