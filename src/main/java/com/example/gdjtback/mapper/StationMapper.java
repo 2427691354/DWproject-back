1e2fa591d030125955dc67d293ff02d61694dae3
18755268664 <2696111076@qq>
2020-01-11 17:36:21 +0800

    接口：修改根据站点id获取站内气瓶最新信息

--- src/main/java/com/example/gdjtback/mapper/StationMapper.java
+++ src/main/java/com/example/gdjtback/mapper/StationMapper.java
@@ -1,6 +1,6 @@
 package com.example.gdjtback.mapper;
 
-import com.example.gdjtback.entity.PressureData;
+
 import com.example.gdjtback.entity.Station;
 import com.example.gdjtback.entity.StationExample;
 
